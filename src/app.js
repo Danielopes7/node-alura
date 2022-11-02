5a5645356aab4d108cf40481832f993c85e720d0
Daniel Amorim <daniel.lopes.amorim@hotmail.com>
2022-11-02 18:23:34 -0300

    adicionando metodo put

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json())
 
 const livros = [
-    {id: 1, "titulo": "Senior dos aneis"},
-    {id: 2, "titulo": "O hobbit"}
+    {id: 1, titulo: "Senior dos aneis"},
+    {id: 2, titulo: "O hobbit"}
 ]
 app.get('/', (req, res) => {
     res.status(200).send('Curso de node');
@@ -20,5 +20,15 @@ app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso')
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.json(livros[index])
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 console.log('teste')
 export default app
