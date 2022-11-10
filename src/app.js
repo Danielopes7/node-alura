3c6c59b50c9e2688303a85d337465eeedcf3ce7c
Danielopes7 <daniel.lopes.amorim@hotmail.com>
2022-11-09 22:57:50 -0300

    apagando metodos nao usados no app.js

--- src/app.js
+++ src/app.js
@@ -10,32 +10,7 @@ db.once("open", () => {
 })
 
 const app = express();
-
 app.use(express.json())
-
 routes(app)
 
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
-})
-
-// app.put('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id)
-//     livros[index].titulo = req.body.titulo
-//     res.json(livros[index])
-// })
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params
-    let index = buscaLivro(id)
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`)
-})
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
-console.log('teste')
 export default app
